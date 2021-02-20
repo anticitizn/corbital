ddeea82e979022862802ac8ef76ab1a3f9ddb40c
Daniel Neykov <metheorita@gmail.com>
2021-02-20 22:41:02 +0100

    Add readme

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#define SDL_MAIN_HANDLED
+#include <iostream>
+#include "SimulationWrapper.h"
+
+using namespace std;
+
+bool running = true;
+
+void processInput(SDL_Event event, SimulationWrapper& sim)
+{
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+			case SDL_MOUSEBUTTONDOWN:
+				int mouseX, mouseY;
+				SDL_GetMouseState(&mouseX, &mouseY);
+				sim.OnClick(mouseX, mouseY);
+			break;
+
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym)
+				{
+					// ESC
+					case 27:
+						running = false;
+					break;
+					
+					case 32: // space
+						sim.Clear();
+					break;
+					
+					case 49: // 1
+						sim.SelectEntity(1);
+					break;
+					
+					case 50: // 2
+						sim.SelectEntity(2);
+					break;
+					
+					case 51: // 3
+						sim.SelectEntity(3);
+					break;
+				}
+			break;
+		}
+	}
+}
+
+int main()
+{
+	// turn all of this event, scale and offset stuff into a class, it's unusable this way
+	SDL_SetRelativeMouseMode(SDL_FALSE);
+	SDL_Event event;
+	SDL_memset(&event, 0, sizeof(event));
+
+	SimulationWrapper sim;
+
+	while (running)
+	{
+		processInput(event, sim);
+		sim.Tick();
+		SDL_Delay(30);
+	}
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# corbital
+
+![corbital_gif](https://user-images.githubusercontent.com/33865403/108609165-7944b280-73cc-11eb-9dff-8113409e924b.gif)
+
+Corbital is an accurate n-body orbital simulator with cool ASCII graphics written in C++. At the current stage the simulations are only two-dimensional and fairly limited in scope, but it will eventually allow the modelling of entire galaxies.
